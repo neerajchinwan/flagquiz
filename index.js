f67cf002f9c2614447ecc90dff58853a442b6c24
neerajchinwan <neeraj14chinwan@gmail.com>
2024-05-01 02:59:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(process.env.PUBLIC_DIR));
+
+
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT, (err) => {
+    if(!err){
+        console.log(`server is running on the port ${process.env.PORT}`)
+    }
+})
